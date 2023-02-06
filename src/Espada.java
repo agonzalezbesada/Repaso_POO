ec2bf9cbe4e18fcf22134b3911f54b0689a38290
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-02-06 19:20:17 +0100

    Ejercicio 1 Terminado

--- src/Espada.java
+++ src/Espada.java
@@ -1,5 +1,13 @@
 public class Espada extends Arma{
 
+    Espada(){
+
+    }
+
+    Espada(int nivel_rareza, int nivel, String nombre, double damage, double multiplicador, double distancia, double tiempo_para_damage) {
+        super(nivel_rareza, nivel, nombre, damage, multiplicador, distancia, tiempo_para_damage);
+    }
+
     public double doing_damage(double distancia_enemigo) {
         if (distancia_enemigo<=getDistancia()) {
             return getMultiplicador() * getDamage() * (getDistancia()-distancia_enemigo);
